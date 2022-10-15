8e20e18a663fecd5a6c7d2eab226c199de618f72
Alexander Fedoseev <alexanderfedoseeff@gmail.com>
2022-10-16 00:27:21 +0300

    #v3

--- app/app.py
+++ app/app.py
@@ -91,4 +91,4 @@ def delete_person(person_id):
     return jsonify({}), 204
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port="8080")
+    app.run(host="0.0.0.0", debug=True, port="80")
